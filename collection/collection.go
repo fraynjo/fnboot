82a0ad4d150a0e5902fe55aac0a9434b2903c224
frayn <frayn_jo@163.com>
2023-01-07 15:26:53 +0800

    add collection

--- collection/collection.go
+++ collection/collection.go
@@ -3,21 +3,101 @@ package collection
 import (
 	"encoding/json"
 	"github.com/fraynjo/fnboot"
+	"github.com/fraynjo/fnboot/array"
+	"github.com/fraynjo/fnboot/lang"
 )
 
-func ForeachArray[T fnboot.FnInterface](arr []T, f func(i int, v T) T) []T {
-	for i, v := range arr {
-		arr[i] = f(i, v)
-	}
-	return arr
-}
-
-func ForeachObj[T fnboot.FnInterface](obj fnboot.FnInterface, f func(k string, v T) T) map[string]T {
-	var m map[string]T
+func ToMap[T fnboot.FnT](obj interface{}) map[T]interface{} {
+	var m map[T]interface{}
 	bs, _ := json.Marshal(obj)
 	_ = json.Unmarshal(bs, &m)
+	return m
+}
+
+func ForeachArray[T fnboot.FnInterface, T1 fnboot.FnInterface](arr []T, f func(index int, item T) T1) []T1 {
+	tempArr := make([]T1, len(arr))
+	for i, v := range arr {
+		tempArr[i] = f(i, v)
+	}
+	return tempArr
+}
+
+func ForeachObj[T fnboot.FnT](obj interface{}, f func(key interface{}, value interface{}) interface{}) map[T]interface{} {
+	m := ToMap[T](obj)
 	for k, v := range m {
 		m[k] = f(k, v)
 	}
 	return m
+}
+
+func ForeachRightArray[T fnboot.FnInterface](arr []T, f func(index int, item T) T) []T {
+	tempArr := make([]T, len(arr))
+	for i := len(arr) - 1; i >= 0; i-- {
+		tempArr[len(arr)-i-1] = f(i, arr[i])
+	}
+	return tempArr
+}
+
+func Every(obj interface{}, f func(key interface{}, item interface{}) bool) bool {
+	if lang.IsArray(obj) {
+		arr := array.InterfaceToArray(obj)
+		return everyArray(arr, f)
+	}
+	if lang.IsMap(obj) {
+		return everyObj(obj, f)
+	}
+	return false
+}
+
+func everyArray(arr []interface{}, f func(key interface{}, item interface{}) bool) bool {
+	for i, v := range arr {
+		if !f(i, v) {
+			return false
+		}
+	}
+	return true
+}
+
+func everyObj(obj interface{}, f func(key interface{}, value interface{}) bool) bool {
+	var m map[string]interface{}
+	bs, _ := json.Marshal(obj)
+	_ = json.Unmarshal(bs, &m)
+	for k, v := range m {
+		if f(k, v) {
+			return true
+		}
+	}
+	return false
+}
+
+func Filter(obj interface{}, f func(key interface{}, item interface{}) bool) interface{} {
+	if lang.IsArray(obj) {
+		arr := array.InterfaceToArray(obj)
+		return everyArray(arr, f)
+	}
+	if lang.IsMap(obj) {
+		return everyObj(obj, f)
+	}
+	return nil
+}
+
+func filterArray(arr []interface{}, f func(key interface{}, item interface{}) bool) interface{} {
+	for i, v := range arr {
+		if f(i, v) {
+			return v
+		}
+	}
+	return nil
+}
+
+func filterObj(obj interface{}, f func(key interface{}, value interface{}) bool) interface{} {
+	var m map[string]interface{}
+	bs, _ := json.Marshal(obj)
+	_ = json.Unmarshal(bs, &m)
+	for k, v := range m {
+		if f(k, v) {
+			return v
+		}
+	}
+	return nil
 }
--- collection/collection_test.go
+++ collection/collection_test.go
@@ -1,15 +1,17 @@
 package collection
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestForeachArray(t *testing.T) {
 	t.Run("number", func(t *testing.T) {
 		arr := []int{1, 2, 3}
-		result := ForeachArray(arr, func(i int, v int) int {
-			return v + 1
+		result := ForeachArray(arr, func(i int, v int) float64 {
+			return float64(v) + float64(1.1)
 		})
 		t.Log(result)
-		if result[0] != 2 {
+		if result[0] != 2.1 {
 			t.Fatal("fail")
 		}
 	})
@@ -21,11 +23,65 @@ func TestForeachObj(t *testing.T) {
 			"name1": "foo1",
 			"name2": "foo2",
 		}
-		result := ForeachObj(obj, func(k string, v string) string {
-			return v + "_" + "a"
+		result := ForeachObj[string](obj, func(k interface{}, v interface{}) interface{} {
+			return v.(string) + "_" + "a"
 		})
 		t.Log(result)
 		if result["name1"] != "foo1_a" {
+			t.Fatal("fail")
+		}
+	})
+}
+
+func TestForeachRightArray(t *testing.T) {
+	t.Run("number", func(t *testing.T) {
+		arr := []int{1, 2, 3}
+		result := ForeachRightArray(arr, func(i int, v int) int {
+			return v + 1
+		})
+		t.Log(result)
+		if result[0] != 4 {
+			t.Fatal("fail")
+		}
+	})
+}
+
+func TestEveryArray(t *testing.T) {
+	t.Run("array", func(t *testing.T) {
+		obj1 := map[string]interface{}{
+			"user":   "barney",
+			"age":    30,
+			"active": false,
+		}
+		obj2 := map[string]interface{}{
+			"user":   "fred",
+			"age":    36,
+			"active": false,
+		}
+		arr := []map[string]interface{}{obj1, obj2}
+		result := Every(arr, func(index interface{}, item interface{}) bool {
+			obj := ToMap[string](item)
+			return !obj["active"].(bool)
+		})
+		t.Log(result)
+		if !result {
+			t.Fatal("fail")
+		}
+	})
+	t.Run("map", func(t *testing.T) {
+		obj := map[string]interface{}{
+			"user":   "barney",
+			"age":    30,
+			"active": false,
+		}
+		result := Every(obj, func(key interface{}, value interface{}) bool {
+			if key == "age" {
+				return value.(float64) == 30
+			}
+			return false
+		})
+		t.Log(result)
+		if !result {
 			t.Fatal("fail")
 		}
 	})
